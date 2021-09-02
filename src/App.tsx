997dd9bd14539e12ec0d3b1467d14390691607c7
Hirofumi INOUE <inouehirofumi1983@gmail.com>
2021-09-02 20:08:25 +0900

    Send message.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import liff from '@line/liff';
 import './App.css';
-import { buildReplyText } from 'line-message-builder';
 
 const App: React.FC = () => {
   const sendMessage = () => {
     liff.init({ liffId: process.env.REACT_APP_LIFF_ID as string}).then(() => {
       if (!liff.isLoggedIn()) {
         liff.login({});
+      } else if (liff.isInClient()) {
+        liff.sendMessages([{
+          'type': 'text',
+          'text': 'send message'
+        }]).then(() => {
+          window.alert('message sent');
+        }).catch((error) => {
+          window.alert('error sending message: ' + error);
+        })
       }
-      liff.sendMessages(buildReplyText(['Send Message']));
     });
   }
   return (
