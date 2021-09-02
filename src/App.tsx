8c7e503694e8a39187ecb4fa3d8310d20a1954df
Hirofumi INOUE <inouehirofumi1983@gmail.com>
2021-09-02 19:49:03 +0900

    send message.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { buildReplyText } from 'line-message-builder';
 
-function App() {
+const App: React.FC = () => {
+  const sendMessage = () => {
+    liff.init({ liffId: process.env.REACT_APP_LIFF_ID as string}).then(() => {
+      if (!liff.isLoggedIn()) {
+        liff.login({});
+      }
+      liff.sendMessages(buildReplyText(['Send Message']));
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          test
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button className='button' onClick={sendMessage}>
+        Send Message
+      </button>
     </div>
-  );
+  )
 }
 
 export default App;
