8ccb95384c3bb67d3b884892c452b650d81d8692
Hirofumi INOUE <inouehirofumi1983@gmail.com>
2021-09-15 20:13:23 +0900

    user info.

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,28 @@ const App: React.FC = () => {
       }
     });
   }
+  const getUserInfo = () => {
+    liff.init({ liffId: process.env.REACT_APP_LIFF_ID as string }).then(() => {
+      if (!liff.isLoggedIn()) {
+        liff.login({});
+      } else if (liff.isInClient()) {
+        liff.getProfile().then(profile => {
+          const userId: string = profile.userId;
+          const displayName: string = profile.displayName;
+          alert(`Name: ${displayName}, userId: ${userId}`);
+        }).catch((error) => {
+          window.alert('error sending message: ' + error);
+        })
+      }
+    })
+  }
   return (
     <div className='App'>
       <button className='button' onClick={sendMessage}>
         Send Message
+      </button>
+      <button className='button' onClick={getUserInfo}>
+        show user info
       </button>
     </div>
   )
